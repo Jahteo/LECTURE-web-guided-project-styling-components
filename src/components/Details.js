46214cc07afdaf72302dd66bec5270e06ece9581
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-13 19:58:43 +0200

    wip

--- src/components/Details.js
+++ src/components/Details.js
@@ -68,7 +68,7 @@ const kf = keyframes`
 
 const StyledDetails = styled.div`
   opacity: 0;
-  animation: ${kf}
+  animation: ${kf} 1s ease-in-out forwards;
   h2 {
     font-size: 3rem;
     background: ${ () => {
