eb7b8efddee74846e356f33f53fd71ac28dab753
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-13 18:37:29 +0200

    wip

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default function Friend({ info, action, actionText }) {
+export default function Friend(props) {
+  const { info, action, actionText } = props
   return (
     <div className='friend'>
       {info.name}
